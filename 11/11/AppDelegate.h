e51c3770c42ed7132a59d2767909ff5fdee5d8ef
zxy <zxy@zxydeimac.local>
2018-03-05 21:09:53 +0800

    111

---
+++ 11/11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  11
+//
+//  Created by zxy on 2018/3/5.
+//  Copyright © 2018年 foxconn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
